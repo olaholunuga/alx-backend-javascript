91343917703578007d35e2efccb532dbd96f4aa1
“olaholunuga” <olaholunuga@gmail.com>
2022-07-27 15:01:54 +0100

    03

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,7 @@
+import { uploadPhoto, createUser } from "./utils"
 function handleProfileSignup() {
-    
+    photo = uploadPhoto.resolve()
+    user = createUser.resolve()
+
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`)
 }
