ea73d1f40b936cfc476aa55eb2542ffd136e354b
“olaholunuga” <olaholunuga@gmail.com>
2022-07-31 02:23:51 +0100

    add 02

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,8 +4,5 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return Error();
-    });
+    .catch(() => new Error());
 }
