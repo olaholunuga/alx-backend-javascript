1f0f991dcab814fe8d2d353003916df8480cc87f
“olaholunuga” <olaholunuga@gmail.com>
2022-07-31 01:59:18 +0100

    00

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,4 @@
-#!/usr/bin/env node
 export default function getResponseFromAPI()
 {
-    // response = new Promise((resolve, reject) => {
-    //   resolve()  
-    // })
     return Promise.resolve()
 }
