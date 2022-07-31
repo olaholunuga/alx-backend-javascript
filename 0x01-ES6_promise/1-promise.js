c1cca827bc10b9f4b4c511cb748423185428d649
“olaholunuga” <olaholunuga@gmail.com>
2022-07-31 02:05:32 +0100

    01 done

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-    let response = new Promise((resolve, reject) => {
-        if (success) {
-            resolve({ status: 200, body: "success"});
-        } else {
-            reject(Error("The fake API is not working currently"));
-        }
-    });
-    return response;
+  const response = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+  return response;
 }
