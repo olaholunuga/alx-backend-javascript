5b93ff73da455baf2ea4ef1c484d292606742340
“olaholunuga” <olaholunuga@gmail.com>
2022-08-03 01:18:56 +0100

    all done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,19 @@
 export default function cleanSet(set, startString) {
   const setArray = [];
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+  if (
+    !set
+    || !startString
+    || !(set instanceof Set)
+    || typeof startString !== 'string'
+  ) {
     return '';
   }
-  if (set instanceof Set) {
-    if (startString) {
-      for (const string of set) {
-        if (string.startsWith(startString)) {
-          setArray.push(string.replace(startString, ''));
-        }
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        setArray.push(valueSubStr);
       }
     }
   }
